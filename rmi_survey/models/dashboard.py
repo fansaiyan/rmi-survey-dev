36098505ae630f2b1b1782d39db17fc148e6b6d3
Rio Ramski <out1689@fifgroup.co.id>
2024-04-13 14:36:36 +0700

    Improve survey system with new dashboard and calculation features
    Implemented comprehensive modifications in the rmi_survey module, which include, among others, the addition of a new Dashboard-Aspect-Dimension function with its associated security settings and database model. A new execute parameter was also added to the SurveyInherit model. Adjustments in the User and Admin group settings were made as part of these changes. Furthermore, new views have been created to exhibit these enhancements to the users. This implementation enhances the survey data collection and examination capabilities, providing more granular and informative insights.

--- rmi_survey/models/dashboard.py
+++ rmi_survey/models/dashboard.py
@@ -6,3 +6,21 @@ class Dashboard(models.Model):
     _description = 'Dashboard RMI Survey Model'
 
     name = fields.Char()
+
+
+class DashboardAspectDimension(models.Model):
+    _name = 'rmi.survey_dashboard_aspect_dimension'
+    _description = 'Dashboard RMI Survey Aspect Dimension Model'
+
+    name = fields.Char()
+    no = fields.Integer(string='No')
+    company = fields.Char(string='Company')
+    question_id = fields.Many2one('survey.question', string='Question')
+    parameter_name = fields.Char(string='Parameter Name')
+    avg_value = fields.Float(string='Average Value')
+    min_value = fields.Float(string='Minimum Value')
+    max_value = fields.Float(string='Maximum Value')
+    range_value = fields.Float(string='Range Value')
+    survey_id = fields.Many2one('survey.survey', string='Survey ID')
+
+
--- rmi_survey/models/models.py
+++ rmi_survey/models/models.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import logging
+
 from odoo import models, fields, api
+_logger = logging.getLogger(__name__)
 
 
 class SurveyInherit(models.Model):
@@ -9,7 +12,56 @@ class SurveyInherit(models.Model):
 
     jenis_industri = fields.Selection([('umum', 'Umum'), ('perbankan', 'Perbankan'), ('asuransi', 'Asuransi')],
                                       'Jenis Industri', default='umum')
-    periode = fields.Date('Periode')
+    periode = fields.Date(string='Periode', widget='year')
+    dashboard_id = fields.Many2one('rmi.survey_dashboard_aspect_dimension', string='Dashboard')
+
+    def generate_adjust_aspect_dimension(self):
+        # Execute custom SQL query
+        query_aspek_dimensi = """
+            select
+                ROW_NUMBER() OVER () AS no,
+                h.name as company,
+                a.question_id as question_id,
+                (b.title->>'en_US')::varchar AS parameterName,
+                avg((e.value->>'en_US')::int) as avgvalue,
+                min((e.value->>'en_US')::int) as minvalue,
+                max((e.value->>'en_US')::int) as maxvalue,
+                max((e.value->>'en_US')::int) - min((e.value->>'en_US')::int) as rangevalue
+                from survey_user_input_line as a
+                left join survey_question as b on a.question_id = b.id
+                left join survey_user_input as c on c.id = a.user_input_id
+                left join res_partner as d on d.id = c.partner_id
+                left join survey_question_answer as e on e.id = a.suggested_answer_id
+                left join res_users as f on f.partner_id = d.id
+                left join hr_employee as g on g.user_id = f.id
+                left join res_company as h on h.id = g.company_id
+            where a.survey_id = {} and c.state = 'done'
+            GROUP BY a.question_id, parameterName, company
+            ORDER BY a.question_id ASC
+        """.format(self.id)
+        self.env.cr.execute(query_aspek_dimensi)
+        fetched_data = self.env.cr.fetchall()
+        # _logger.info(fetched_data)
+        # Iterate over fetched_data and create records
+        for record_data in fetched_data:
+            # Create a new record
+            self.env['rmi.survey_dashboard_aspect_dimension'].create({
+                'no': record_data[0],
+                'company': record_data[1],
+                'question_id': record_data[2],
+                'parameter_name': record_data[3],
+                'avg_value': record_data[4],
+                'min_value': record_data[5],
+                'max_value': record_data[6],
+                'range_value': record_data[7],
+                'name': self.title
+                # Add more fields as needed
+            })
+
+    def action_view_source_dashboard_form(self):
+        self.ensure_one()
+        result = self.env['ir.actions.act_window']._for_xml_id('rmi_survey.survey_dashboard_dimension_action')
+        return result
 
 
 class SurveyQuestionInherit(models.Model):
