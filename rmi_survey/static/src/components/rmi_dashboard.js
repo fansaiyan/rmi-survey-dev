705060f53ab2308d36f1cd5d7b649c5b73a68b8b
Rio Ramski <out1689@fifgroup.co.id>
2024-04-20 22:12:34 +0700

    Get Filter Survey Function in rmi_dashboard.js

--- rmi_survey/static/src/components/rmi_dashboard.js
+++ rmi_survey/static/src/components/rmi_dashboard.js
@@ -1,13 +1,35 @@
 /** @odoo-module */
 
 import { registry } from "@web/core/registry"
+import { useService } from "@web/core/utils/hooks"
 import { KpiCard } from "./kpi_card/kpi_card"
 import { ChartRenderer } from "./chart_renderer/chart_renderer"
-const { Component } = owl
+
+const { Component, useState, onWillStart } = owl
 
 export class RmiDashboard extends Component {
     setup(){
+        this.state = useState({
+            surveyFilter: 0,
+            selectedFilterSurvey: null,
+        })
+        this.orm = useService("orm")
 
+        onWillStart(async ()=> {
+            await this.getFilteredSurvey()
+        })
+    }
+
+    async getFilteredSurvey(){
+        // let domain = [['active', '!=', ['false']]];
+        const data = await this.orm.readGroup('survey.survey', [], ["id"], ["title"]);
+        console.log('data ->', data)
+        let filteredData = data.map(item => {
+            const value = item.title;
+            const totalAmount = item.service_total;
+        });
+        console.log(filteredData)
+        this.state.selectedFilterSurvey = filteredData
     }
 }
 
